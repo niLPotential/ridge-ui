b90e4647b91146813f9281d6d58675fd134d1c66
niLPotential <niLPotential@proton.me>
2025-07-21 05:43:23 +0000

    Add state

--- src/component.ts
+++ src/component.ts
@@ -1,4 +1,5 @@
 import type {
+  Bindable,
   BindableContext,
   ComputedFn,
   Machine,
@@ -8,16 +9,18 @@ import type {
 import { useMachine } from "./lib/machine.ts";
 
 export class Component<T extends MachineSchema> {
+  state: Bindable<T["state"]>;
   send: (event: any) => void;
   context: BindableContext<T>;
   prop: PropFn<T>;
   computed: ComputedFn<T>;
 
   constructor(machine: Machine<T>, userProps: Partial<T["props"]> = {}) {
-    const { send, context, prop, computed, init } = useMachine(
+    const { state, send, context, prop, computed, init } = useMachine(
       machine,
       userProps,
     );
+    this.state = state;
     this.send = send;
     this.context = context;
     this.prop = prop;
