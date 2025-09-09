06b069eabd2fa53578a754951a85bd072f1713ce
niLPotential <niLPotential@proton.me>
2025-09-09 06:52:57 +0000

    Add module documentation

--- packages/lib/mod.ts
+++ packages/lib/mod.ts
@@ -1,2 +1,17 @@
+/**
+ * Implements a basic Zag state machine constructor for Alpine.js.
+ * Direct use is not intended. Use `@ridge-ui/*` components instead.
+ *
+ * @example
+ * ```ts
+ * import * as component from "@zag-js/component";
+ * import { AlpineMachine } from "@ridge-ui/lib";
+ *
+ * export class Component extends AlpineMachine<component.Schema> {...}
+ * ```
+ *
+ * @module
+ */
+
 export { mergeProps } from "@zag-js/core";
 export { AlpineMachine } from "./machine.ts";
