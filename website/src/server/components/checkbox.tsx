3a754f0393997dfe53388747931d3203919f18e3
nilpotential <nilpotential@proton.me>
2025-09-10 00:02:14 +0900

    Change colors and label order

--- website/src/server/components/checkbox.tsx
+++ website/src/server/components/checkbox.tsx
@@ -2,27 +2,27 @@ export default function CheckBoxDemo() {
   return (
     <div x-data="{props: {disabled: false, invalid: false}}">
       <div>
-        <label for="disabled">disabled</label>
         <input type="checkbox" id="disabled" x-model="props.disabled" />
+        <label for="disabled">disabled</label>
       </div>
       <div>
-        <label for="invalid">invalid</label>
         <input type="checkbox" id="invalid" x-model="props.invalid" />
+        <label for="invalid">invalid</label>
       </div>
       <label
         x-data="checkbox(props)"
         x-bind="root"
-        class="flex flex-row-reverse gap-2 select-none text-lg data-[disabled]:(cursor-not-allowed opacity-40)"
+        class="flex flex-row gap-2 select-none text-lg data-[disabled]:(cursor-not-allowed opacity-40)"
       >
+        <div
+          x-bind="control"
+          class="w-24px h-24px rounded-0.275rem border-solid border-2 border-blue text-white data-[hover]:bg-gray data-[focus]:(outline-2 outline-solid outline-blue outline-offset-2) data-[disabled]:(bg-black border-black) data-[invalid]:border-red data-[state=indeterminate]:(bg-white border-yellow text-yellow) data-[state=checked]:(bg-blue border-blue)"
+        >
+        </div>
         <span x-bind="label">
           Input is
           <span x-text="checked ? ' checked' : ' unchecked'"></span>
         </span>
-        <div
-          x-bind="control"
-          class="w-24px h-24px rounded-0.275rem border-solid border-2 border-green text-white data-[hover]:bg-gray-2 data-[focus]:(outline-2 outline-solid outline-red outline-offset-2) data-[disabled]:(bg-black border-blue) data-[invalid]:border-yellow data-[state=indeterminate]:(bg-white border-gray text-gray) data-[state=checked]:(bg-red border-red)"
-        >
-        </div>
         <input x-bind="hiddenInput" />
       </label>
     </div>
