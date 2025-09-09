925a39d65743760c750175b0190ceddca9036339
niLPotential <niLPotential@proton.me>
2025-09-09 05:31:18 +0000

    Follow svelte package

--- packages/lib/track.ts
+++ packages/lib/track.ts
@@ -1,8 +1,30 @@
 import Alpine from "alpinejs";
+import { isEqual, isFunction } from "@zag-js/utils";
 
-export const useTrack = (deps: any[], effect: VoidFunction) => {
-  Alpine.reactive([...deps.map((d) => d())]);
+function access(value: any) {
+  if (isFunction(value)) return value();
+  return value;
+}
+
+export const track = (deps: any[], effect: VoidFunction) => {
+  let prevDeps: any[] = [];
+  let isFirstRun = true;
   Alpine.effect(() => {
-    effect();
+    if (isFirstRun) {
+      prevDeps = deps.map((d) => access(d));
+      isFirstRun = false;
+      return;
+    }
+    let changed = false;
+    for (let i = 0; i < deps.length; i++) {
+      if (!isEqual(prevDeps[i], deps[i])) {
+        changed = true;
+        break;
+      }
+    }
+    if (changed) {
+      prevDeps = deps.map((d) => access(d));
+      effect();
+    }
   });
 };
