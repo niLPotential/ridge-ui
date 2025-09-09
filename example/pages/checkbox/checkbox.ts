15811f957f07a0f5936a4ab1c2aed22ee590ffad
niLPotential <niLPotential@proton.me>
2025-09-09 05:32:49 +0000

    Declare data before global assignment

--- example/pages/checkbox/checkbox.ts
+++ example/pages/checkbox/checkbox.ts
@@ -2,9 +2,7 @@ import "virtual:uno.css";
 import Alpine from "alpinejs";
 import { Checkbox } from "@ridge-ui/checkbox";
 
+Alpine.data("checkbox", (userProps: any) => new Checkbox(userProps));
 // @ts-ignore: Alpine
 globalThis.Alpine = Alpine;
-
-Alpine.data("checkbox", (userProps: any) => new Checkbox(userProps));
-
 Alpine.start();
