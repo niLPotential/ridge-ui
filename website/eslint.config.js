29d784bb7ed14158cb3a2a1570f51057e7eb91ad
niLPotential <niLPotential@proton.me>
2025-09-10 05:02:15 +0000

    Simplify eslint config

--- website/eslint.config.js
+++ website/eslint.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "eslint/config";
-import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import unocss from "@unocss/eslint-config/flat";
 
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  unocss,
-);
+export default defineConfig({
+  files: ["**/*.tsx"],
+  languageOptions: {
+    parser: tseslint.parser,
+  },
+}, unocss);
