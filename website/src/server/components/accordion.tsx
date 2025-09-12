b621025f46dac87ee2701f45bcc6d68bbd1703c5
niLPotential <niLPotential@proton.me>
2025-09-12 02:26:53 +0000

    Add styling

--- website/src/server/components/accordion.tsx
+++ website/src/server/components/accordion.tsx
@@ -4,17 +4,23 @@ export default function AccordionDemo() {
       <template
         x-for="value in ['First', 'Second', 'Third']"
         x-bind:key="value"
+        class="w-md"
       >
-        <div x-bind="item({value})">
+        <div x-bind="item({value})" class="border-1 bg-truegray text-sm">
           <h3>
             <button
               type="button"
               x-bind="itemTrigger({value})"
               x-text="value + ' Item'"
+              class="w-full cursor-pointer bg-blueGray px-3 py-2 text-align-start focus-visible:(outline-2px outline-blue outline-solid)"
             >
             </button>
           </h3>
-          <div x-bind="itemContent({value})" x-text="value + ' content'">
+          <div
+            x-bind="itemContent({value})"
+            x-text="value + ' content'"
+            class="w-full px-3 py-2"
+          >
           </div>
         </div>
       </template>
