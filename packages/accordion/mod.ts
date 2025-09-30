a723f6098fdfc1f6121adb73dc195b9701688747
nilpotential <nilpotential@proton.me>
2025-09-30 22:11:00 +0900

    add module docs

--- packages/accordion/mod.ts
+++ packages/accordion/mod.ts
@@ -1,2 +1,31 @@
+/**
+ * Alpine.js accordion plugin powered by Zag
+ *
+ * @example
+ * ```html
+ *  <div x-accordion="props" x-accordion:root>
+ *    <div x-accordion:item="{value}">
+ *      <h3>
+ *        <button x-accordion:item-trigger="{value}"></button>
+ *      </h3>
+ *      <div x-accordion:item-content></div>
+ *    </div>
+ *  </div>
+ * ```
+ *
+ * @module
+ */
+
 import Accordion from "./plugin.ts";
+
+/**
+ * Alpine.js accordion plugin powered by Zag
+ *
+ * ```ts
+ *  import Alpine from "alpinejs"
+ *  import Accordion from "@ridge-ui/accordion"
+ *
+ *  Alpine.plugin(Accordion)
+ * ```
+ */
 export default Accordion;
